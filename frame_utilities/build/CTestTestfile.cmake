02adfe66ed874ee29f2ad977eabb2f2f0fefde2d
Jordan Lack <jlack1987@gmail.com>
2015-09-07 20:43:06 -0500

    Removed accidental commit of build directory. Added .gitignore.

--- frame_utilities/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jordan/Git/catkin_workspace/src/mobility_utilities/frame_utilities
-# Build directory: /home/jordan/Git/catkin_workspace/src/mobility_utilities/frame_utilities/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(_ctest_frame_utilities_gtest_frame_utilities-test "/home/jordan/Git/catkin_workspace/src/mobility_utilities/frame_utilities/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/indigo/share/catkin/cmake/test/run_tests.py" "/home/jordan/Git/catkin_workspace/src/mobility_utilities/frame_utilities/build/test_results/frame_utilities/gtest-frame_utilities-test.xml" "--return-code" "/home/jordan/Git/catkin_workspace/src/mobility_utilities/frame_utilities/build/devel/lib/frame_utilities/frame_utilities-test --gtest_output=xml:/home/jordan/Git/catkin_workspace/src/mobility_utilities/frame_utilities/build/test_results/frame_utilities/gtest-frame_utilities-test.xml")
