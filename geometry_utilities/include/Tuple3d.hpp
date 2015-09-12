0d9b08e115790251be0efe11220f59ec624a3ad9
Jordan Lack <jlack1987@gmail.com>
2015-09-12 10:53:56 -0500

    Adding more methods to Tuple3d.

--- geometry_utilities/include/Tuple3d.hpp
+++ geometry_utilities/include/Tuple3d.hpp
@@ -15,8 +15,31 @@ class Tuple3d
 		Tuple3d(double array[3]);
 		Tuple3d(std::vector<double> vector);
 
-	private:
+		void set(const double &x, const double &y, const double &z);
+		void set(double array[3]);
+		void set(std::vector<double> vector);
+
+		void add(const Tuple3d &tuple);
+		void add(const double &x, const double &y, const double &z);
+		void add(const Tuple3d &tuple1, const Tuple3d &tuple2);
+
+		void subtract(const Tuple3d &tuple);
+		void subtract(const double &x, const double &y, const double &z);
+		void subtract(const Tuple3d &tuple1, const Tuple3d &tuple2);
+
+		void negate();
+		void negate(const Tuple3d &tuple);
+
+		void scale(const double &value);
+		void scale(const double &value, const Tuple3d &tuple);
+
+		void scaleAdd(const double &value, const Tuple3d &tuple);
+		void scaleAdd(const double &value, const Tuple3d &tuple1, const Tuple3d &tuple2);
+
 		double x, y, z;
+
+	private:
+
 };
 }
 
--- geometry_utilities/src/Tuple3d.cpp
+++ geometry_utilities/src/Tuple3d.cpp
@@ -4,9 +4,7 @@ namespace geometry_utilities
 {
 Tuple3d::Tuple3d()
 {
-	this->x = 0;
-	this->y = 0;
-	this->z = 0;
+	set(0.0, 0.0, 0.0);
 }
 
 Tuple3d::Tuple3d(const Tuple3d &tupleToCopy)
@@ -18,16 +16,12 @@ Tuple3d::Tuple3d(const Tuple3d &tupleToCopy)
 
 Tuple3d::Tuple3d(const double &x, const double &y, const double &z)
 {
-	this->x = x;
-	this->y = y;
-	this->z = z;
+	set(x, y, z);
 }
 
 Tuple3d::Tuple3d(double array[3])
 {
-	this->x = array[0];
-	this->y = array[1];
-	this->z = array[2];
+	set(array[0], array[1], array[2]);
 }
 
 Tuple3d::Tuple3d(std::vector<double> vector)
@@ -37,9 +31,104 @@ Tuple3d::Tuple3d(std::vector<double> vector)
 		throw std::runtime_error("Vector size is not equal to 3!");
 	}
 
-	this->x = vector[0];
-	this->y = vector[1];
-	this->z = vector[2];
+	set(vector[0], vector[1], vector[2]);
 }
 
+void Tuple3d::set(const double &x, const double &y, const double &z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+void Tuple3d::set(std::vector<double> vector)
+{
+	set(vector[0], vector[1], vector[2]);
+}
+
+void Tuple3d::set(double array[3])
+{
+	set(array[0], array[1], array[2]);
+}
+
+void Tuple3d::add(const Tuple3d &tuple)
+{
+	this->x += tuple.x;
+	this->y += tuple.y;
+	this->z += tuple.z;
+}
+
+void Tuple3d::add(const double &x, const double &y, const double &z)
+{
+	this->x += x;
+	this->y += y;
+	this->z += z;
+}
+
+void Tuple3d::add(const Tuple3d &tuple1, const Tuple3d &tuple2)
+{
+	this->x = tuple1.x + tuple2.x;
+	this->y = tuple1.y + tuple2.y;
+	this->z = tuple1.z + tuple2.z;
+}
+
+void Tuple3d::subtract(const Tuple3d &tuple)
+{
+	this->x -= tuple.x;
+	this->y -= tuple.y;
+	this->z -= tuple.z;
+}
+
+void Tuple3d::subtract(const double &x, const double &y, const double &z)
+{
+	this->x -= x;
+	this->y -= y;
+	this->z -= z;
+}
+
+void Tuple3d::subtract(const Tuple3d &tuple1, const Tuple3d &tuple2)
+{
+	this->x = tuple1.x - tuple2.x;
+	this->y = tuple1.y - tuple2.y;
+	this->z = tuple1.z - tuple2.z;
+}
+
+void Tuple3d::negate()
+{
+	this->x *= -1;
+	this->y *= -1;
+	this->z *= -1;
+}
+
+void Tuple3d::negate(const Tuple3d &tuple)
+{
+	set(tuple.x, tuple.y, tuple.z);
+	negate();
+}
+
+void Tuple3d::scale(const double &value)
+{
+	this->x *= value;
+	this->y *= value;
+	this->z *= value;
+}
+
+void Tuple3d::scale(const double &value, const Tuple3d &tuple)
+{
+	set(tuple.x, tuple.y, tuple.z);
+	scale(value);
+}
+
+void Tuple3d::scaleAdd(const double &value, const Tuple3d &tuple)
+{
+	scale(value);
+	add(tuple);
+}
+
+void Tuple3d::scaleAdd(const double &value, const Tuple3d &tuple1, const Tuple3d &tuple2)
+{
+	set(tuple1.x, tuple1.y, tuple1.z);
+	scale(value);
+	add(tuple2);
+}
 }
