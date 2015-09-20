99185ede54a0cee15a20ee502fa331e3565ba331
Jordan Lack <jlack1987@gmail.com>
2015-09-20 13:39:34 -0500

    Just more stuff on FramePoint.

--- frame_utilities/include/FramePoint.hpp
+++ frame_utilities/include/FramePoint.hpp
@@ -7,6 +7,11 @@ class FramePoint : public FrameTuple
 {
 	public:
 		FramePoint();
+		FramePoint(const std::string &name, ReferenceFrame* referenceFrame, const double &x, const double &y, const double &z);
+		FramePoint(const std::string &name, ReferenceFrame* referenceFrame, double array[3]);
+		FramePoint(const std::string &name, ReferenceFrame* referenceFrame, std::vector<double> vector);
+		FramePoint(const FramePoint &framePoint);
+		FramePoint(const std::string &name, ReferenceFrame* referenceFrame);
 };
 
 }
