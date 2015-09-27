40f53e8c97e6d564dbb185cca16c11e1eaa8f1ca
Jordan Lack <jlack1987@gmail.com>
2015-09-27 16:20:02 -0500

    Removed a bunch of comments from test.

--- geometry_utilities/test/RigidBodyTransformTest.cpp
+++ geometry_utilities/test/RigidBodyTransformTest.cpp
@@ -107,30 +107,21 @@ TEST_F(RigidBodyTransformTest, testNormalize)
 
 TEST_F(RigidBodyTransformTest, testUseQuaternions_1)
 {
-	// tf2::Quaternion quat1 = GeometryUtilitiesTestHelper::createRandomQuaternion();
-	Quaternion quat1(0.4219074509830524, 0.16571163801219513, 0.8514918861954333, 0.26361965703574125);
-	// tf2::Quaternion quat2 = quat1;
-	// quat2.normalize();
-	// std::cout << quat2.getAxis().getX() << "," << quat2.getAxis().getY() << "," << quat2.getAxis().getZ() << "," << quat2.getW() << std::endl;
-	RigidBodyTransform transform;
-	transform.setRotationAndZeroTranslation(quat1);
+	for (int i = 0; i < nTests; i++)
+	{
+		Quaternion quat1(0.4219074509830524, 0.16571163801219513, 0.8514918861954333, 0.26361965703574125);
 
-	// // std::cout << transform << std::endl;
-	// // -0.50499756  - 0.30911005   0.80587123 |  0.00000000
-	// // 0.58876995  - 0.80608866   0.05975804 |  0.00000000
-	// // 0.63113185   0.50465043   0.58906751
+		RigidBodyTransform transform;
+		transform.setRotationAndZeroTranslation(quat1);
 
+		Quaternion quatToCheck;
+		transform.getRotation(quatToCheck);
 
-
-	Quaternion quatToCheck;
-	transform.getRotation(quatToCheck);
-
-	// std::cout << quatToCheck.getAxis().getX() << "," << quatToCheck.getAxis().getY() << "," << quatToCheck.getAxis().getZ() << "," << quatToCheck.getW() << std::endl;
-
-	ASSERT_TRUE(fabs(quatToCheck.getX() - quat1.getX()) < 1e-5);
-	ASSERT_TRUE(fabs(quatToCheck.getY() - quat1.getY()) < 1e-5);
-	ASSERT_TRUE(fabs(quatToCheck.getZ() - quat1.getZ()) < 1e-5);
-	ASSERT_TRUE(fabs(quatToCheck.getW() - quat1.getW()) < 1e-5);
+		ASSERT_TRUE(fabs(quatToCheck.getX() - quat1.getX()) < 1e-5);
+		ASSERT_TRUE(fabs(quatToCheck.getY() - quat1.getY()) < 1e-5);
+		ASSERT_TRUE(fabs(quatToCheck.getZ() - quat1.getZ()) < 1e-5);
+		ASSERT_TRUE(fabs(quatToCheck.getW() - quat1.getW()) < 1e-5);
+	}
 }
 
 }
