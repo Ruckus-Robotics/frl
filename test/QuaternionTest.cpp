0160fd0247ca01cfa752f8de1b3a1ef7efe0ada3
Jordan Lack <jlack1987@gmail.com>
2015-10-19 20:44:06 -0500

    Adding Quaternion test.

---
+++ test/QuaternionTest.cpp
@@ -0,0 +1,25 @@
+#include "geometry_utilities/Quaternion.hpp"
+#include "GeometryUtilitiesTestHelper.hpp"
+
+namespace geometry_utilities
+{
+
+class QuaternionTest : public ::testing::Test
+{
+	protected:
+
+		virtual void SetUp()
+		{
+
+		}
+		virtual void TearDown()
+		{
+
+		}
+
+		int nTests = 1000;
+};
+
+
+
+}
